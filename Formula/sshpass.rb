89b18b01847feaddbe5193739630f0448ea6fa7f
dewab <dwhicker@bifrost.cc>
2023-04-15 10:06:39 -0500

    removed name tag

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -4,22 +4,22 @@ class Sshpass < Formula
   version "1.10"
   sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
 
-  url "http://sourceforge.net/projects/sshpass/files/sshpass/#{version}/sshpass-#{version}.gz"
-  name "sshpass"
+  url "https://downloads.sourceforge.net/project/sshpass/sshpass/#{version}/sshpass-#{version}.tar.gz"
   desc "Non-interactive ssh password authentication"
   homepage "http://sourceforge.net/projects/sshpass"
   license "GPL-2.0-or-later"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", 
+                "--disable-debug", 
+                "--disable-dependency-tracking",
+                "--prefix=#{prefix}"
     system "make install"
   end
 
   test do
     assert_match "ssh: Could not resolve hostname host: nodename nor servname provided, or not known",
       shell_output("#{bin}/sshpass -p mypassword ssh username@host touch foo 2>&1", 255)
-
     assert_match "sshpass #{version}", shell_output("#{bin}/sshpass -V")
   end
 end
